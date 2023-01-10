3e19ad2c8fc0602a78199f3522bca5e93237a435
arokiabenitoraj <benitoraj98@gmail.com>
2023-01-10 13:08:34 +0530

    hello

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ class hello{
  public static void main(String args[]){
  System.out.println("hello my world");
   System.out.println("hello our world");
+System.out.println("hello all world");
+
 }
 }
